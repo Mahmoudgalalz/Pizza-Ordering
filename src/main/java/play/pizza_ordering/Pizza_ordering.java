66f01d7264ffbf194efb73d84210fe90f521fe6b
sondos <sondosmabdelnaser@gmail.com>
2022-05-14 23:43:08 +0200

    done

--- src/main/java/play/pizza_ordering/Pizza_ordering.java
+++ src/main/java/play/pizza_ordering/Pizza_ordering.java
@@ -5,6 +5,6 @@ package play.pizza_ordering;
 public class Pizza_ordering {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("tttt");
     }
 }
